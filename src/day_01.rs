0d7146427e5a189d463e3e9175aa48bb982333f4
FractalArt <FractalArt@users.noreply.github.com>
2024-12-03 12:04:13 +0100

    Clean Up Day 2.

--- src/day_01.rs
+++ src/day_01.rs
@@ -5,32 +5,44 @@ use std::collections::HashMap as HM;
 
 /// The solution to task 1 of day 1.
 pub fn day_01_1(data: &[String]) -> i32 {
-    let (mut v1, mut v2) = data.iter()
+    let (mut v1, mut v2) = data
+        .iter()
         .map(|s| s.split_once("   ").unwrap())
         .map(|(x1, x2)| (x1.parse::<i32>().unwrap(), x2.parse::<i32>().unwrap()))
-        .fold((Vec::with_capacity(data.len()), Vec::with_capacity(data.len())), |(mut v1, mut v2), (x1, x2)| {v1.push(x1); v2.push(x2); (v1, v2)});
+        .fold(
+            (
+                Vec::with_capacity(data.len()),
+                Vec::with_capacity(data.len()),
+            ),
+            |(mut v1, mut v2), (x1, x2)| {
+                v1.push(x1);
+                v2.push(x2);
+                (v1, v2)
+            },
+        );
     v1.sort();
     v2.sort();
-    v1.into_iter().zip(v2)
-        .map(|(x1, x2)| (x1 - x2).abs())
-        .sum()
+    v1.into_iter().zip(v2).map(|(x1, x2)| (x1 - x2).abs()).sum()
 }
 
 /// The solution to task 2 of day 1.
 pub fn day_01_2(data: &[String]) -> u32 {
-    let (v, hm) = data.iter()
+    let (v, hm) = data
+        .iter()
         .map(|s| s.split_once("   ").unwrap())
         .map(|(x1, x2)| (x1.parse::<u32>().unwrap(), x2.parse::<u32>().unwrap()))
-            .fold((Vec::with_capacity(data.len()), HM::<u32, u32>::new()), |(mut v, mut hm), (x1, x2)| {
-                hm.entry(x2).and_modify(|counter| *counter += 1).or_insert(1);
+        .fold(
+            (Vec::with_capacity(data.len()), HM::<u32, u32>::new()),
+            |(mut v, mut hm), (x1, x2)| {
+                hm.entry(x2)
+                    .and_modify(|counter| *counter += 1)
+                    .or_insert(1);
                 v.push(x1);
                 (v, hm)
-            });
-    v.into_iter()
-        .map(|x| x * hm.get(&x).unwrap_or(&0))
-        .sum()
+            },
+        );
+    v.into_iter().map(|x| x * hm.get(&x).unwrap_or(&0)).sum()
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -61,5 +73,4 @@ mod tests {
         ];
         assert_eq!(day_01_2(&data), 31);
     }
-
 }
--- src/day_02.rs
+++ src/day_02.rs
@@ -5,91 +5,71 @@ use itertools::Itertools;
 use std::hash::Hash;
 
 #[derive(PartialEq, Hash, Eq)]
-enum Report {
+enum Diff {
     ValidIncreasing,
     ValidDecreasing,
     Invalid,
 }
 
-/// The solution to task 1 of day 2.
-pub fn day_02_1(data: &[String]) -> usize {
-    data.iter()
-        .map(|report| report.split_whitespace()
-                            .map(|x| x.parse::<i32>().unwrap())
-                            .tuple_windows()
-                            .map(|(x, y)| match x-y {
-                                d if d.abs() > 0 && d.abs() < 4 && d < 0 => Report::ValidDecreasing,
-                                d if d.abs() > 0 && d.abs() < 4 && d > 0 => Report::ValidIncreasing,
-                                _ => Report::Invalid
-                            })
-                            .all_equal()
-
-            )
-        .filter(|&r| r)
-        .count()
-}
-
-#[derive(Debug, PartialEq)]
-enum Rep {
-    Valid,
-    Invalid,
-    PotentiallyFixable,
-}
-
-fn validate_report(report: &[i32]) -> Rep {
-    let  counts = report.iter()
+fn valid_report(report: &[i32]) -> bool {
+    let counts = report
+        .iter()
         .tuple_windows()
-        .map(|(x, y)| match x-y {
-            d if d.abs() > 0 && d.abs() < 4 && d < 0 => Report::ValidDecreasing,
-            d if d.abs() > 0 && d.abs() < 4 && d > 0 => Report::ValidIncreasing,
-            _ => Report::Invalid
+        .map(|(x, y)| match x - y {
+            d if d.abs() > 0 && d.abs() < 4 && d < 0 => Diff::ValidDecreasing,
+            d if d.abs() > 0 && d.abs() < 4 && d > 0 => Diff::ValidIncreasing,
+            _ => Diff::Invalid,
         })
         .counts();
 
-        match (*counts.get(&Report::Invalid).unwrap_or(&0), *counts.get(&Report::ValidIncreasing).unwrap_or(&0), *counts.get(&Report::ValidDecreasing).unwrap_or(&0)) {
-            (0, 0, _d) => Rep::Valid,
-            (0, _d, 0) => Rep::Valid,
-            (1, 0, _d) => Rep::PotentiallyFixable,
-            (1, _d, 0) => Rep::PotentiallyFixable,
-            (0, 1, _d) => Rep::PotentiallyFixable,
-            (0, _d, 1) => Rep::PotentiallyFixable,
-            _ => Rep::Invalid,
-
-        }
-
-
+    matches!(
+        (
+            *counts.get(&Diff::Invalid).unwrap_or(&0),
+            *counts.get(&Diff::ValidIncreasing).unwrap_or(&0),
+            *counts.get(&Diff::ValidDecreasing).unwrap_or(&0)
+        ),
+        (0, 0, _d) | (0, _d, 0)
+    )
 }
 
-
 fn try_fix(report: &[i32]) -> bool {
-    for i in 0..report.len() {
-        let mut clone: Vec<i32> = report.to_vec();
-        clone.remove(i);
-        if validate_report(&clone) == Rep::Valid {
-            return true
-        }
+    report
+        .iter()
+        .enumerate()
+        .any(|(i, _)| valid_report(&[&report[..i], &report[i + 1..]].concat()))
+}
 
-    }
-    false
+/// The solution to task 1 of day 2.
+pub fn day_02_1(data: &[String]) -> usize {
+    data.iter()
+        .map(|report| {
+            valid_report(
+                &report
+                    .split_whitespace()
+                    .map(|x| x.parse::<i32>().unwrap())
+                    .collect::<Vec<_>>(),
+            )
+        })
+        .filter(|&r| r)
+        .count()
 }
 
 /// The solution to task 2 of day 2.
 pub fn day_02_2(data: &[String]) -> usize {
     data.iter()
         .map(|report| {
-            let report: Vec<_>  = report.split_whitespace()
-                            .map(|x| x.parse::<i32>().unwrap())
-                            .collect();
-            match validate_report(&report) {
-                Rep::Valid => true,
-                Rep::Invalid => try_fix(&report),
-                Rep::PotentiallyFixable => try_fix(&report)
+            let report: Vec<_> = report
+                .split_whitespace()
+                .map(|x| x.parse::<i32>().unwrap())
+                .collect();
+            match valid_report(&report) {
+                true => true,
+                false => try_fix(&report),
             }
         })
         .filter(|&r| r)
         .count()
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -109,13 +89,13 @@ mod tests {
     }
 
     #[test]
-    fn test_validate_report() {
-        assert_eq!(validate_report(&[7, 6, 4, 2, 1]), Rep::Valid);
-        assert_eq!(validate_report(&[1, 2, 7, 8, 9]), Rep::PotentiallyFixable);
-        assert_eq!(validate_report(&[9, 7, 6, 2, 1]), Rep::PotentiallyFixable);
-        assert_eq!(validate_report(&[1, 3, 2, 4, 5]), Rep::PotentiallyFixable);
-        assert_eq!(validate_report(&[8, 6, 4, 4, 1]), Rep::PotentiallyFixable);
-        assert_eq!(validate_report(&[1, 3, 6, 7, 9]), Rep::Valid);
+    fn test_valid_report() {
+        assert!(valid_report(&[7, 6, 4, 2, 1]));
+        assert!(!valid_report(&[1, 2, 7, 8, 9]));
+        assert!(!valid_report(&[9, 7, 6, 2, 1]));
+        assert!(!valid_report(&[1, 3, 2, 4, 5]));
+        assert!(!valid_report(&[8, 6, 4, 4, 1]));
+        assert!(valid_report(&[1, 3, 6, 7, 9]));
     }
 
     #[test]
@@ -125,7 +105,7 @@ mod tests {
         assert!(try_fix(&[1, 3, 2, 4, 5]));
         assert!(try_fix(&[8, 6, 4, 4, 1]));
     }
-    
+
     #[test]
     fn test_day_02_2() {
         let data = [
@@ -138,5 +118,4 @@ mod tests {
         ];
         assert_eq!(day_02_2(&data), 4);
     }
-
 }
